8396ee7a9069339e380bc4ea148d1a7164165c2b
aheig <61814388+aheig@users.noreply.github.com>
2023-03-03 18:15:08 +0800

    Create upxray.sh

---
+++ upxray.sh
@@ -0,0 +1,14 @@
+#! /bin/sh
+
+# 仅适用于AMD64架构路由器升级Xray
+
+echo   注意：仅适用于AMD64架构路由器升级Xray
+
+
+cd /tmp
+read -rp "请输入版本号：" Ver
+wget https://github.com/XTLS/Xray-core/releases/download/v$Ver/Xray-linux-64.zip
+unzip Xray*
+chmod +rx xray
+cp /usr/bin/xray /usr/bin/xray.bak
+mv xray /usr/bin/
